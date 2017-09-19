18de5effca5d7fdfb57621b4b9d220d706f5fe76
rkrishnabh <rkrishnabh@gmail.com>
2017-09-19 17:07:02 +0530

    v7

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 // grab the packages we need
 var express = require('express');
+var nforce =require('nforce');
 var app = express();
+//var sfuser=process.env.SFUSER;
+//var sfpass= pass.env.SFPASS;
+
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+//connect to salesforce
+var org=nforce.createConnection({
+	clientId:process.env.ConsumerKey,
+	cllentSecret:process.env.ConsumerSecret,
+	redirectUri:'http://localhost:3000/oauth/_callback',
+	mode:'single'
+	});
+	org.authenicate({username:sfuser,password:sfpass},function(err,resp){
+	if(!err){
+	console.log('connected')
+	}else{
+	console.log('Error connecting to salesforce' + err.message);
+	}
+	});
 
 // POST http://localhost:8080/api/users
 // parameters sent with 
@@ -16,6 +33,7 @@ app.put('/api/users', function(req, res) {
 
     res.send(tempreading);
 });
+
 
 // start the server
 app.listen(port);
