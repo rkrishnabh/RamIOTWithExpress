cac005ca3b7452899eda221e24df09cc4bea63e5
rkrishnabh <rkrishnabh@gmail.com>
2017-09-20 13:38:43 +0530

    v12

--- server.js
+++ server.js
@@ -16,20 +16,13 @@ var nforce = require('nforce'),
 //var sfuser=process.env.SFUSER;
 //var sfpass= pass.env.SFPASS;
 var port = process.env.PORT || 3000;
+//server.listen(port);
+var oauth;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-
-// POST http://localhost:8080/api/users
-// parameters sent with 
-app.put('/api/users', function(req, res) {
-	
-	 var tempreading = req.body.tempdata.value;
-   console.log('tempreading'+tempreading);
-
-    res.send(tempreading);
-	 var org=nforce.createConnection({
+//connecting to salesforce
+ var org=nforce.createConnection({
 	 clientId:process.env.CLII,
 	 clientSecret:process.env.CLIS,
 	 redirectUri:'https://login.salesforce.com/',
@@ -40,12 +33,73 @@ app.put('/api/users', function(req, res) {
 	 });
 	 org.authenticate({ username:user,password:pass,loginUri:login},function(err, resp)
 	 {if(!err){
-		console.log('connected')
+		console.log('connected');
+		 console.log('Access Token...: ' + resp.access_token);
+		 oauth = resp;
+		 console.log('oauth ' + oauth);
+		 
 		}else{
 			console.log('Error connecting to salesforce:::::'+err.message);
 		}
 	 }
 	 );
+
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.put('/api/users', function(req, res) {
+	
+	 var tempreading = req.body.tempdata.value;
+   console.log('tempreading::::::'+tempreading);
+   
+   var org=nforce.createConnection({
+	 clientId:process.env.CLII,
+	 clientSecret:process.env.CLIS,
+	 redirectUri:'https://login.salesforce.com/',
+	 apiVersion: 'v40.0',
+	 environment: 'production',
+	 mode:'multi',
+	 plugins:['tooling']
+	 });
+	 //IOT__c creation 
+	 function insertLead() {
+  console.log('Attempting to insert IOT__c');
+  //var voltage='99999999';
+  var ld = nforce.createSObject('IOT__c', {
+    Voltage__c: tempreading
+   
+  });
+  org.insert({ sobject: ld, oauth: oauth }, function(err, resp) {
+    if(err) {
+      console.error('--> unable to insert lead');
+      console.error('--> ' + JSON.stringify(err));
+    } else {
+      console.log('--> IOT__c inserted');
+      
+    }
+  });
+}
+
+console.log('Authenticating with Salesforce');
+
+	 // authenticate using username-password oauth flow
+	 org.authenticate({ username:user,password:pass,loginUri:login},function(err, resp)
+	 {if(!err){
+		console.log('connected');
+		console.log('Access Token...: ' + resp.access_token);
+		 oauth = resp;
+		 insertLead();
+		}else{
+			console.log('Error connecting to salesforce:::::'+err.message);
+		}
+	 }
+	 );
+	 
+   
+   
+
+    res.send();
+	
 	
 });
 
