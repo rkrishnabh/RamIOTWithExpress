f5c78df01d3e690adb931adcd6b7b809836e8fe3
rkrishnabh <rkrishnabh@gmail.com>
2017-09-20 10:57:16 +0530

    v11

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // grab the packages we need
 var express = require('express');
-var nforce =require('nforce');
 var app = express();
-var sfuser=process.env.SFUSER;
-var sfpass= process.env.SFPASS;
-
+var nforce = require('nforce'),
+	tooling = require('nforce-tooling')(nforce);
+	var dotenv = require('dotenv');
+	dotenv.load();
+	var user= 'rama.bhatraju@gmail.com';
+	var pass=process.env.PASS;
+	var login=process.env.uri;
+	console.log('userName'+user);
+	console.log('pass'+pass);
+	console.log('login'+login);
+	console.log('process.env.CLII'+process.env.CLII);
+	console.log('process.env.CLIS'+process.env.CLIS);
+//var sfuser=process.env.SFUSER;
+//var sfpass= pass.env.SFPASS;
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-//connect to salesforce
-console.log('sfuser'+sfuser);
-console.log('sfpass'+sfpass);
+
 
 // POST http://localhost:8080/api/users
 // parameters sent with 
@@ -21,8 +29,25 @@ app.put('/api/users', function(req, res) {
    console.log('tempreading'+tempreading);
 
     res.send(tempreading);
+	 var org=nforce.createConnection({
+	 clientId:process.env.CLII,
+	 clientSecret:process.env.CLIS,
+	 redirectUri:'https://login.salesforce.com/',
+	 apiVersion: 'v40.0',
+	 environment: 'production',
+	 mode:'multi',
+	 plugins:['tooling']
+	 });
+	 org.authenticate({ username:user,password:pass,loginUri:login},function(err, resp)
+	 {if(!err){
+		console.log('connected')
+		}else{
+			console.log('Error connecting to salesforce:::::'+err.message);
+		}
+	 }
+	 );
+	
 });
-
 
 // start the server
 app.listen(port);
