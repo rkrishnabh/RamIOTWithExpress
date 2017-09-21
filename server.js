c179586b8048c2fd41ecbe3462624f8826c794d1
rkrishnabh <rkrishnabh@gmail.com>
2017-09-21 13:31:43 +0530

    v14

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var nforce = require('nforce'),
 var dotenv = require('dotenv');
 dotenv.load();
 //Variable declaration for sales force connecting
-	var user= 'rama.bhatraju@gmail.com';
+	var user= 'truservdemo@birlasoft.com';
 	var pass=process.env.PASS;
 	var login=process.env.uri;
 //port information for run in locally	
@@ -27,51 +27,50 @@ app.put('/sfIOTIntegration', function(req, res) {
 	voltage = req.body.voltage.value;	 
       console.log('Voltage::::::'+voltage);
       authentication();
-	  
       console.log('Authenticating with Salesforce');
       res.send();
 
 });
-
-function authentication(){
-	org=nforce.createConnection({
-	 clientId:process.env.CLII,
-	 clientSecret:process.env.CLIS,
-	 redirectUri:'https://login.salesforce.com/',
-	 apiVersion: 'v40.0',
-	 environment: 'production',
-	 mode:'multi',
-	 plugins:['tooling']
-	 });
- // authenticate using username-password oauth flow
-	 org.authenticate({ username:user,password:pass,loginUri:login},function(err, resp)
-	 {if(!err){
-		console.log('connected');
-		console.log('Access Token...: ' + resp.access_token);
-		 oauth = resp;
-		 insertVoltage();
-		}else{
-			console.log('Error connecting to salesforce:::::'+err.message);
+	//Connecting to salesforce
+	function authentication(){
+		org=nforce.createConnection({
+		 clientId:process.env.CLII,
+		 clientSecret:process.env.CLIS,
+		 redirectUri:'https://login.salesforce.com/',
+		 apiVersion: 'v40.0',
+		 environment: 'production',
+		 mode:'multi',
+		 plugins:['tooling']
+		 });
+	 // authenticate using username-password oauth flow
+		 org.authenticate({ username:user,password:pass,loginUri:login},function(err, resp)
+		 {if(!err){
+			console.log('connected');
+			console.log('Access Token...: ' + resp.access_token);
+			 oauth = resp;
+			 insertVoltage();
+			}else{
+				console.log('Error connecting to salesforce:::::'+err.message);
+			}
+		 }
+		 );
+	}
+        //create record creation in sales force
+		function insertVoltage() {
+		console.log('Attempting to create record in to salesforce');
+		var iot = nforce.createSObject('tsdm__IoT_Light_Sensor__c', {
+		tsdm__Voltage__c: voltage
+		});
+	  org.insert({ sobject: iot, oauth: oauth }, function(err, resp) {
+		if(err) {
+		  console.error('--> unable to insert iot');
+		  console.error('--> ' + JSON.stringify(err));
+		} else {
+			
+		  console.log('--> sensor reading inserted successfully in salesforce');
+		  
 		}
-	 }
-	 );
-}
-//create record creation in sales force
-	function insertVoltage() {
-	console.log('Attempting to create record in IOT__c');
-	var iot = nforce.createSObject('IOT__c', {
-	Voltage__c: voltage
-	});
-  org.insert({ sobject: iot, oauth: oauth }, function(err, resp) {
-    if(err) {
-      console.error('--> unable to insert iot');
-      console.error('--> ' + JSON.stringify(err));
-    } else {
-		
-      console.log('--> sensor reading inserted successfully in salesforce');
-      
-    }
-  });
-}
+	  });
+	}
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
